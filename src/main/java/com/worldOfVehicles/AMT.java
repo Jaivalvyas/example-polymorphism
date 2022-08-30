b5b658803b56c3b488ebf10e0672e15683a18ac9
Jaival <jaival.s.vyas@gmail.com>
2022-08-30 16:25:39 +0530

    created showSpec method in AMT class

--- src/main/java/com/worldOfVehicles/AMT.java
+++ src/main/java/com/worldOfVehicles/AMT.java
@@ -5,5 +5,29 @@
  */
 package com.worldOfVehicles;
 
-public class AMT {
+public class AMT extends Transmission {
+    void showSpecs(String s, String s1, int forward_gears, double first_gear_ratio, double second_gear_ratio, double third_gear_ratio, double fourth_gear_ratio) {
+        System.out.println("Transmission Type: " + s);
+        System.out.println("Transmission Model: " + s1);
+
+        System.out.println("Key Specifications: ");
+        System.out.println("1. Forward Gears: " + forward_gears);
+        System.out.println("2. 1st Gear ration: " + first_gear_ratio);
+        System.out.println("3. 2st Gear ration: " + second_gear_ratio);
+        System.out.println("4. 3st Gear ration: " + third_gear_ratio);
+        System.out.println("5. 4st Gear ration: " + fourth_gear_ratio);
+    }
+
+    void showSpecs(String s, String s1, int forward_gears, double first_gear_ratio, double second_gear_ratio, double third_gear_ratio, double fourth_gear_ratio, double fifth_gear_ratio) {
+        System.out.println("Transmission Type: " + s);
+        System.out.println("Transmission Model: " + s1);
+
+        System.out.println("Key Specifications: ");
+        System.out.println("1. Forward Gears: " + forward_gears);
+        System.out.println("2. 1st Gear ration: " + first_gear_ratio);
+        System.out.println("3. 2nd Gear ration: " + second_gear_ratio);
+        System.out.println("4. 3rd Gear ration: " + third_gear_ratio);
+        System.out.println("5. 4th Gear ration: " + fourth_gear_ratio);
+        System.out.println("6. 5th Gear ration: " + fifth_gear_ratio);
+    }
 }
