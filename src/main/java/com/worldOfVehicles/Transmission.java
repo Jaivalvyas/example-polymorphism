b629d16aa47aa68ba1928aeaaea9f21f99316ec5
Jaival <jaival.s.vyas@gmail.com>
2022-08-30 16:00:19 +0530

    created Transmission class with attributes

---
+++ src/main/java/com/worldOfVehicles/Transmission.java
@@ -0,0 +1,30 @@
+/*
+ * Author Name: Jaival
+ * Date: 30-08-2022
+ * Createdd with IntelliJ IDEA Community Editiion
+ */
+package com.worldOfVehicles;
+
+public class Transmission {
+    int forward_gears;
+    double first_gear_ratio;
+    double second_gear_ratio;
+    double third_gear_ratio;
+    double fourth_gear_ratio;
+    double fifth_gear_ratio;
+    double sixth_gear_ratio;
+    double seventh_gear_ratio;
+    double eighth_gear_ratio;
+
+    public Transmission(int forward_gears, double first_gear_ratio, double second_gear_ratio, double third_gear_ratio, double fourth_gear_ratio, double fifth_gear_ratio, double sixth_gear_ratio, double seventh_gear_ratio, double eighth_gear_ratio) {
+        this.forward_gears = forward_gears;
+        this.first_gear_ratio = first_gear_ratio;
+        this.second_gear_ratio = second_gear_ratio;
+        this.third_gear_ratio = third_gear_ratio;
+        this.fourth_gear_ratio = fourth_gear_ratio;
+        this.fifth_gear_ratio = fifth_gear_ratio;
+        this.sixth_gear_ratio = sixth_gear_ratio;
+        this.seventh_gear_ratio = seventh_gear_ratio;
+        this.eighth_gear_ratio = eighth_gear_ratio;
+    }
+}
